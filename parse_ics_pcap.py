0663cad3bbbf78a27a94da77d130e2758856a5db
amanda <amanda.huang@lionic.com>
2024-05-29 13:16:20 +0800

    Add fields to be parsed for S7+ and OPCUA, and refactor the code.

--- parse_ics_pcap.py
+++ parse_ics_pcap.py
@@ -17,18 +17,159 @@ def find_pcap_files(folder_path):
     pcap_files = glob.glob(os.path.join(folder_path, '*'))
     return pcap_files
 
-def filter_packets(input_pcap, pkt_layer, protocol, port_num, display_filter, **kwargs):
-
-    packets = pyshark.FileCapture(input_pcap, display_filter= display_filter)
-    
+def filter_s7_plus_packets(input_pcap, pkt_layer, protocol, port_num, display_filter, **kwargs):
+    packets = pyshark.FileCapture(input_pcap, display_filter=display_filter)
     packet_to_save = []
-    pcap_dict = {input_pcap : {}}
+    pcap_dict = {input_pcap: {}}
     idx = 0
     for packet in packets:
         pkt_data = {}
-        # opcua 封包可能會有reassembled的封包, 需要確認 packet.data.tcp_segment 是否存在
-        # 其他協定可能也有data這個屬性，但有可能是 COTP reassembled
-        reassembled_result = [] 
+        if 'TCP' in str(packet.layers):
+            pkt_data['L4'] = 'TCP'
+            src_port_num = packet.tcp.srcport
+            dest_port_num = packet.tcp.dstport
+        elif 'UDP' in str(packet.layers):
+            pkt_data['L4'] = 'UDP'
+            src_port_num = packet.udp.srcport
+            dest_port_num = packet.udp.dstport
+
+        pkt_data['srcport'] = src_port_num
+        pkt_data['destport'] = dest_port_num
+
+        if dest_port_num == port_num:
+            direction = "query"
+            pkt_data['direction'] = direction
+        else:
+            direction = "response"
+            pkt_data['direction'] = direction
+
+        protocol_value = packet[protocol]
+
+        if protocol_value and hasattr(protocol_value, 'get_field_by_showname'):
+            key_field_obj = protocol_value.get_field_by_showname(kwargs.get('key_field_1'))
+
+            if key_field_obj and protocol == 's7comm-plus':
+                key_field_data = key_field_obj.show
+                value_to_convert = int(key_field_data, 16)
+                key_field_name_1 = kwargs.get('key_field_name_1')
+                key_field_obj_2 = protocol_value.get_field_by_showname(kwargs.get('key_field_2'))
+                key_field_data_2 = key_field_obj_2.show
+
+                if key_field_data_2 == "0x01":
+                    key_field_data_2 = "v1"
+                elif key_field_data_2 == "0x02":
+                    key_field_data_2 = "v2"
+                elif key_field_data_2 == "0x03":
+                    key_field_data_2 = "v3"
+
+                key_field_name_2 = kwargs.get('key_field_name_2')
+                dir_and_func = f'{direction},{key_field_name_1}={key_field_data},{key_field_name_2}={key_field_data_2}'
+            
+            if dir_and_func not in seen_values:
+                seen_values.add(dir_and_func)
+
+                packet_value = packet.frame_info
+                if packet_value and hasattr(packet_value, 'get_field_by_showname'):
+                    frame_value = packet_value.get_field_by_showname("Frame Number")
+                    if frame_value:
+                        frame_num = frame_value.show
+                        pkt_data['frame_num'] = frame_num
+                        
+                        if protocol == 's7comm-plus':
+                            pcap_filename_data = f'{direction},{key_field_name_1}={key_field_data},({value_to_convert})_{key_field_data_2}.pcap'
+                        
+                        pkt_data["pcap_filename_data"] = pcap_filename_data
+                        packet_to_save.append(pkt_data)
+                        pkt = {idx: pkt_data}
+                        pcap_dict[input_pcap].update(pkt)
+                        idx = idx + 1
+
+    ics_pcap_info.update(pcap_dict)
+
+    return packet_to_save
+
+def filter_opc_ua_packets(input_pcap, pkt_layer, protocol, port_num, display_filter, **kwargs):
+    packets = pyshark.FileCapture(input_pcap, display_filter=display_filter)
+    packet_to_save = []
+    pcap_dict = {input_pcap: {}}
+    idx = 0
+    for packet in packets:
+        pkt_data = {}
+        if 'TCP' in str(packet.layers):
+            pkt_data['L4'] = 'TCP'
+            src_port_num = packet.tcp.srcport
+            dest_port_num = packet.tcp.dstport
+        elif 'UDP' in str(packet.layers):
+            pkt_data['L4'] = 'UDP'
+            src_port_num = packet.udp.srcport
+            dest_port_num = packet.udp.dstport
+
+        pkt_data['srcport'] = src_port_num
+        pkt_data['destport'] = dest_port_num
+
+
+        if dest_port_num not in port_num:
+            direction = "query"
+            pkt_data['direction'] = direction
+        else:
+            direction = "response"
+            pkt_data['direction'] = direction
+
+        protocol_value = packet[protocol]
+
+        if protocol_value and hasattr(protocol_value, 'get_field_by_showname'):
+            key_field_obj_1 = protocol_value.get_field_by_showname(kwargs.get('key_field_1'))
+            key_field_obj_2 = protocol_value.get_field_by_showname(kwargs.get('key_field_2'))
+            message_type_field = protocol_value.get_field_by_showname("Message Type")
+            if message_type_field:
+                message_type = message_type_field.show
+                if hasattr(packet, 'data'):
+                    reassembled_pcap_nums = [int(i.show) for i in packet.data.tcp_segment.all_fields]
+                    pkt_data['reassembled'] = reassembled_pcap_nums
+            if key_field_obj_2 and protocol == 'opcua':
+                if hasattr(packet, 'data'):
+                    reassembled_pcap_nums = [int(i.show) for i in packet.data.tcp_segment.all_fields]
+                    pkt_data['reassembled'] = reassembled_pcap_nums
+                key_field_data = int(key_field_obj_2.show)
+                value_to_convert = hex(key_field_data)
+                key_field_name_1 = kwargs.get('key_field_name_1')
+                key_field_name_2 = kwargs.get('key_field_name_2')
+                dir_and_func = f'{direction},{key_field_name_1}={message_type},{key_field_name_2}={key_field_data}'
+                # print(f'dir_and_func= {dir_and_func}...')
+            elif protocol == 'opcua':
+                key_field_name_1 = kwargs.get('key_field_name_1')
+                dir_and_func = f'{direction},{key_field_name_1}={message_type}'
+                # print(f'dir_and_func= {dir_and_func}...')
+            if dir_and_func not in seen_values:
+                seen_values.add(dir_and_func)
+
+                packet_value = packet.frame_info
+                if packet_value and hasattr(packet_value, 'get_field_by_showname'):
+                    frame_value = packet_value.get_field_by_showname("Frame Number")
+                    if frame_value:
+                        frame_num = frame_value.show
+                        pkt_data['frame_num'] = frame_num
+                        if key_field_obj_2 and protocol == 'opcua':
+                            pcap_filename_data = f'{direction},{key_field_name_1}={message_type},{key_field_name_2}={key_field_data},({value_to_convert}).pcap'
+                        elif protocol == 'opcua':
+                            pcap_filename_data = f'{direction},{key_field_name_1}={message_type}.pcap'
+                        pkt_data["pcap_filename_data"] = pcap_filename_data
+                        packet_to_save.append(pkt_data)
+                        pkt = {idx: pkt_data}
+                        pcap_dict[input_pcap].update(pkt)
+                        idx = idx + 1
+
+    ics_pcap_info.update(pcap_dict)
+
+    return packet_to_save
+
+def filter_melsec_packets(input_pcap, pkt_layer, protocol, port_num, display_filter, **kwargs):
+    packets = pyshark.FileCapture(input_pcap, display_filter=display_filter)
+    packet_to_save = []
+    pcap_dict = {input_pcap: {}}
+    idx = 0
+    for packet in packets:
+        pkt_data = {}
 
         if 'TCP' in str(packet.layers):
             pkt_data['L4'] = 'TCP'
@@ -38,81 +179,68 @@ def filter_packets(input_pcap, pkt_layer, protocol, port_num, display_filter, **
             pkt_data['L4'] = 'UDP'
             src_port_num = packet.udp.srcport
             dest_port_num = packet.udp.dstport
-        
+
         pkt_data['srcport'] = src_port_num
         pkt_data['destport'] = dest_port_num
-
-        # 判斷封包方向, 依據不同的協定，封包方向的判斷方式不同
-        if protocol != 'melsec_communication':
-            if dest_port_num == port_num:
-                direction = "request"
-                pkt_data['direction'] = direction
-            else:
-                direction = "response"
-                pkt_data['direction'] = direction
 
         protocol_value = packet[protocol]
 
         if protocol_value and hasattr(protocol_value, 'get_field_by_showname'):
             key_field_obj = protocol_value.get_field_by_showname(kwargs.get('key_field_1'))
 
-            if key_field_obj and protocol == 'opcua':
-                if hasattr(packet, 'data'):
-                    reassembled_pcap_nums = reassembled_pcap_nums_str = [int(i.show) for i in packet.data.tcp_segment.all_fields]
-                    reassembled_result.extend(reassembled_pcap_nums)
-                key_field_data = int(key_field_obj.show)
-                value_to_convert = hex(key_field_data) # 將 10 進位轉換成 16 進位
-                key_field_name_1 = kwargs.get('key_field_name_1')
-                dir_and_func = f'{direction},{key_field_name_1}={key_field_data}'
-            elif key_field_obj and protocol == 's7comm-plus':
-                key_field_data = key_field_obj.show
-                value_to_convert = int(key_field_data, 16) # 將 16 進位轉換成 10 進位
-                key_field_name_1 = kwargs.get('key_field_name_1')
-                dir_and_func = f'{direction},{key_field_name_1}={key_field_data}'
-            elif key_field_obj and protocol == 'melsec_communication':                    
+            if key_field_obj and protocol == 'melsec_communication':
                 subheader = protocol_value.get_field_by_showname(kwargs.get('subheader_magic')).show
                 if subheader == "0x5000":
-                    direction = "request"
+                    direction = "query"
+                    frame_type = "3E"
+                    pkt_data['frame_type'] = frame_type
                     pkt_data['direction'] = direction
-                elif subheader == "0xd000":
+                elif subheader == "0x5400":
+                    direction = "query"
+                    frame_type = "4E"
+                    pkt_data['frame_type'] = frame_type
+                    pkt_data['direction'] = direction
+                elif subheader == "0xd000" or subheader == "0xd400":
                     direction = "response"
+                    frame_type = "3E"
+                    pkt_data['frame_type'] = frame_type
                     pkt_data['direction'] = direction
-                
+                elif subheader == "0xd400":
+                    direction = "response"
+                    frame_type = "4E"
+                    pkt_data['frame_type'] = frame_type
+                    pkt_data['direction'] = direction                    
+
                 key_field_data = key_field_obj.show
-                value_to_convert = int(key_field_data, 16) 
+                value_to_convert = int(key_field_data, 16)
                 key_field_obj_2 = protocol_value.get_field_by_showname(kwargs.get('key_field_2'))
                 key_field_data_2 = key_field_obj_2.show
                 value_to_convert_2 = int(key_field_data_2, 16)
-                
+
                 key_field_name_1 = kwargs.get('key_field_name_1')
                 key_field_name_2 = kwargs.get('key_field_name_2')
 
-                dir_and_func = f'{direction},{key_field_name_1}={key_field_data},{key_field_name_2}={key_field_data_2}'
-            
-            if dir_and_func not in seen_values: # 避免重複處理相同的封包
+                dir_and_func = f'{direction},{key_field_name_1}={key_field_data},{key_field_name_2}={key_field_data_2},{frame_type}'
+
+            if dir_and_func not in seen_values:
                 seen_values.add(dir_and_func)
 
-                # 找出封包編號
-                packet_value = packet.frame_info # 取得封包資訊
+                packet_value = packet.frame_info
                 if packet_value and hasattr(packet_value, 'get_field_by_showname'):
                     frame_value = packet_value.get_field_by_showname("Frame Number")
                     if frame_value:
                         frame_num = frame_value.show
-                        # 如果有reassembled的封包，則將reassembled的封包編號加入pkt_data
-                        if reassembled_result != []:
-                            pkt_data['reassembled'] = reassembled_result
                         pkt_data['frame_num'] = frame_num
-                        
-                        if protocol == 'opcua' or protocol == 's7comm-plus':
-                            pcap_filename_data = f'{direction},{key_field_name_1}={key_field_data},({value_to_convert}).pcap'
-                        elif protocol == 'melsec_communication':
-                            pcap_filename_data = f'{direction},CommandCode={key_field_data},SubCommandCode={key_field_data_2}(CommandCode={value_to_convert},SubCommandCode={value_to_convert_2}).pcap'
+
+                        if protocol == 'melsec_communication':
+                            pcap_filename_data = f'{frame_type},{direction},CommandCode={key_field_data},SubCommandCode={key_field_data_2}(Command={value_to_convert},SubCommand={value_to_convert_2}).pcap'                        
                         
                         pkt_data["pcap_filename_data"] = pcap_filename_data
                         packet_to_save.append(pkt_data)
-                        pkt = { idx: pkt_data} 
+                        pkt = {idx: pkt_data}
                         pcap_dict[input_pcap].update(pkt)
                         idx = idx + 1
+
     ics_pcap_info.update(pcap_dict)
 
     return packet_to_save
@@ -156,16 +284,16 @@ for input_pcap in pcap_files:
     
     # 根據不同的協定，過濾封包
     if protocol == 's7comm-plus':
-        pcap_info_array = filter_packets(input_pcap, **ics_info['s7comm-plus'])
+        pcap_info_array = filter_s7_plus_packets(input_pcap, **ics_info['s7comm-plus'])
     elif protocol == 'opcua':
-        pcap_info_array = filter_packets(input_pcap, **ics_info['opcua'])
+        pcap_info_array = filter_opc_ua_packets(input_pcap, **ics_info['opcua'])
     elif protocol == 'melsec_communication':
-        pcap_info_array = filter_packets(input_pcap, **ics_info['melsec_communication'])
+        pcap_info_array = filter_melsec_packets(input_pcap, **ics_info['melsec_communication'])
 
     for pkt_data in pcap_info_array: # 將過濾後的封包存成新的 pcap 檔案
         # 確認pkt_data是否有reassembled這個key
         if 'reassembled' in pkt_data:
-            reassembled_pcap_nums = pkt_data['reassembled']
+            reassembled_pcap_nums = pkt_data['reassembled'] 
             conditions = " or ".join([f"frame.number == {num}" for num in reassembled_pcap_nums])
             display_filter_str = conditions
             print(f"display_filter_str= {display_filter_str}...")
